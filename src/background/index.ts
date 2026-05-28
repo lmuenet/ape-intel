c9456dfe5cb5ba5afbbd5765c577d752f6e9656f
l95m <lmueller95@gmx.net>
2026-05-28 14:24:24 +0200

    feat(background): wire apewisdom service into message router

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,6 +1,18 @@
+import { fetchApewisdomSnapshot } from "../lib/apewisdom";
+import { browserStorageKvStore } from "../lib/kv-store";
 import { fetchTickerFromOpenFigi } from "../lib/openfigi";
+import { createApewisdomService } from "./apewisdom-service";
 import { handleMessage } from "./messages";
 
+const store = browserStorageKvStore(browser.storage.local);
+const apewisdom = createApewisdomService(store, () =>
+  fetchApewisdomSnapshot(fetch),
+);
+
 browser.runtime.onMessage.addListener((message) =>
-  handleMessage(message, (isin) => fetchTickerFromOpenFigi(isin, fetch)),
+  handleMessage(
+    message,
+    (isin) => fetchTickerFromOpenFigi(isin, fetch),
+    (ticker) => apewisdom.lookup(ticker),
+  ),
 );
