e14f40a95c976a0148c45f349a783741a6388cfd
l95m <lmueller95@gmx.net>
2026-05-28 20:49:00 +0200

    feat(background): wire tradestie + stocktwits into router

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,18 +1,23 @@
 import { fetchApewisdomSnapshot } from "../lib/apewisdom";
 import { browserStorageKvStore } from "../lib/kv-store";
 import { fetchTickerFromOpenFigi } from "../lib/openfigi";
+import { fetchStockTwitsForTicker } from "../lib/stocktwits";
+import { fetchTradestieSnapshot } from "../lib/tradestie";
 import { createApewisdomService } from "./apewisdom-service";
+import { createStockTwitsService } from "./stocktwits-service";
+import { createTradestieService } from "./tradestie-service";
 import { handleMessage } from "./messages";
 
 const store = browserStorageKvStore(browser.storage.local);
-const apewisdom = createApewisdomService(store, () =>
-  fetchApewisdomSnapshot(fetch),
-);
+const apewisdom = createApewisdomService(store, () => fetchApewisdomSnapshot(fetch));
+const tradestie = createTradestieService(store, () => fetchTradestieSnapshot(fetch));
+const stocktwits = createStockTwitsService(store, (ticker) => fetchStockTwitsForTicker(ticker, fetch));
 
 browser.runtime.onMessage.addListener((message) =>
-  handleMessage(
-    message,
-    (isin) => fetchTickerFromOpenFigi(isin, fetch),
-    (ticker) => apewisdom.lookup(ticker),
-  ),
+  handleMessage(message, {
+    fetchTicker: (isin) => fetchTickerFromOpenFigi(isin, fetch),
+    lookupApewisdom: (ticker) => apewisdom.lookup(ticker),
+    lookupTradestie: (ticker) => tradestie.lookup(ticker),
+    lookupStockTwits: (ticker) => stocktwits.lookup(ticker),
+  }),
 );
