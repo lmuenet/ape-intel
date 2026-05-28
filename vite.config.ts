0ffd5879ccf8be25fa6d6c6a8c48d3b37ca7861d
l95m <lmueller95@gmx.net>
2026-05-28 11:51:24 +0200

    build: vite + crxjs plugin for firefox mv3

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { crx } from "@crxjs/vite-plugin";
+import { defineConfig } from "vite";
+import manifest from "./manifest.config";
+
+export default defineConfig({
+  plugins: [
+    crx({
+      manifest,
+      browser: "firefox",
+    }),
+  ],
+  test: {
+    environment: "jsdom",
+    globals: true,
+  },
+});
